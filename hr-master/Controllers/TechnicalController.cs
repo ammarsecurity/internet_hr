5b92ff75ac3068759c270da30b0ccda08e2d6f07
Ammarrashad <30632722+ammarsecurity@users.noreply.github.com>
2020-10-28 23:19:13 +0300

    Add
    DoneTask

--- hr-master/Controllers/TechnicalController.cs
+++ hr-master/Controllers/TechnicalController.cs
@@ -97,6 +97,84 @@ namespace hr_master.Controllers
 
 
         [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetMyAllTask([FromQuery] PaginationFilter filter, string Task_Employee_WorkOn, DateTime? date, string Task_Employee_Open, int? Task_Status)
+        {
+
+
+            string currentUserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var _clientid = Guid.Parse(currentUserId);
+
+
+            var validFilter = new PaginationFilter(filter.PageNumber, filter.PageSize);
+            var totalRecords = await _context.Tasks.Where(x => x.IsDelete == false && x.Task_Employee_Open == _clientid && x.Task_Status != 3).CountAsync();
+
+
+            var list = (from task in _context.Tasks.OrderByDescending(x => x.Task_Date).Where(x => x.IsDelete == false && x.Task_Employee_Open == _clientid && x.Task_Status != 3).AsNoTracking()
+                        join Employee1 in _context.EmployessUsers.AsNoTracking() on task.Task_Employee_Open equals Employee1.Id
+                        join tower in _context.Towers.AsNoTracking() on task.Tower_Id equals tower.Id into vtower
+                        from tower in vtower.DefaultIfEmpty()
+                        join Employee in _context.EmployessUsers.AsNoTracking() on task.Task_Employee_WorkOn equals Employee.Id into vEmployee
+                        from Employee in vEmployee.DefaultIfEmpty()
+                        join Employee2 in _context.EmployessUsers.AsNoTracking() on task.Task_Employee_Close equals Employee2.Id into vEmployee2
+                        from Employee2 in vEmployee2.DefaultIfEmpty()
+                        join part in _context.Teams.AsNoTracking() on task.Task_part equals part.Id
+                        join reward in _context.RewardsTable.AsNoTracking() on task.Task_Price_rewards equals reward.Id into vRewards
+                        from reward in vRewards.DefaultIfEmpty()
+                        join internetuser in _context.InternetUsers on task.InternetUserId equals internetuser.Id into vinternetuser
+                        from internetuser in vinternetuser.DefaultIfEmpty()
+
+
+                        select new TasksDto
+                        {
+                            Id = task.Id,
+                            Task_Title = task.Task_Title,
+                            Task_part = part.Team_Name,
+                            Task_Status = task.Task_Status,
+                            Task_closed_Note = task.Task_closed_Note,
+                            Task_Date = task.Task_Date,
+                            Task_Done = task.Task_Done,
+                            Task_Employee_Close = Employee2.Employee_Fullname ?? "انتظار موظف",
+                            Task_Employee_Open = Employee1.Employee_Fullname,
+                            Task_Employee_WorkOn = Employee.Employee_Fullname ?? "انتظار موظف",
+                            Task_EndDate = task.Task_EndDate,
+                            Task_Note = task.Task_Note,
+                            Task_Open = task.Task_Open,
+                            Task_Price_rewards = task.Task_Price_rewards,
+                            Tower_Name = tower.Tower_Name ?? "لايوجد",
+                            Tower_Id = tower.Id,
+                            part_Id = part.Id,
+                            Task_Price = reward.RewardsPrice,
+                            InternetUserId = internetuser.Id,
+
+
+
+
+
+                        }).ToList();
+
+            if (Task_Employee_WorkOn != null && Task_Employee_WorkOn != default)
+                list = list.Where(s => s.Task_Employee_WorkOn.Contains(Task_Employee_WorkOn)).ToList();
+            totalRecords = list.Count();
+            if (Task_Employee_Open != null && Task_Employee_Open != default)
+                list = list.Where(s => s.Task_Employee_Open.Contains(Task_Employee_Open)).ToList();
+            totalRecords = list.Count();
+            if (date != null && date != default)
+                list = list.Where(s => s.Task_Date.Date == date).ToList();
+            totalRecords = list.Count();
+            if (Task_Status != null && Task_Status != default)
+                list = list.Where(s => s.Task_Status == Task_Status).ToList();
+            totalRecords = list.Count();
+
+            return Ok(new PagedResponse<List<TasksDto>>(
+                list.Skip((validFilter.PageNumber - 1) * validFilter.PageSize).Take(validFilter.PageSize).ToList(),
+                validFilter.PageNumber,
+                validFilter.PageSize,
+                totalRecords, 0, 0));
+
+
+        }
+
+        [HttpGet]
         public async Task<ActionResult<IEnumerable<string>>> GetAllTask([FromQuery] PaginationFilter filter, string Task_Employee_WorkOn, DateTime? date, string Task_Employee_Open, int? Task_Status)
         {
 
@@ -114,10 +192,10 @@ namespace hr_master.Controllers
             var teams = await _context.Teams.Where(x => x.Id == Employeeinfo.Employee_Team).FirstOrDefaultAsync();
 
             var validFilter = new PaginationFilter(filter.PageNumber, filter.PageSize);
-            var totalRecords = await _context.Tasks.Where(x => x.IsDelete == false && x.Task_part == teams.Id).CountAsync();
+            var totalRecords = await _context.Tasks.Where(x => x.IsDelete == false && x.Task_part == teams.Id && x.Task_Status != 3).CountAsync();
 
 
-            var list = (from task in _context.Tasks.OrderByDescending(x => x.Task_Date).Where(x => x.IsDelete == false && x.Task_part == teams.Id).AsNoTracking()
+            var list = (from task in _context.Tasks.OrderByDescending(x => x.Task_Date).Where(x => x.IsDelete == false && x.Task_part == teams.Id && x.Task_Status != 3).AsNoTracking()
                         join Employee1 in _context.EmployessUsers.AsNoTracking() on task.Task_Employee_Open equals Employee1.Id
                         join tower in _context.Towers.AsNoTracking() on task.Tower_Id equals tower.Id into vtower
                         from tower in vtower.DefaultIfEmpty()
@@ -182,24 +260,31 @@ namespace hr_master.Controllers
 
         }
 
-
-
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<string>>> GetAllMyTask([FromQuery] PaginationFilter filter, string Task_Employee_WorkOn, DateTime? date, string Task_Employee_Open, int? Task_Status)
+        public async Task<ActionResult<IEnumerable<string>>> GetAllDoneTask([FromQuery] PaginationFilter filter, string Task_Employee_WorkOn, DateTime? date, string Task_Employee_Open, int? Task_Status)
         {
 
 
             string currentUserId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
             var _clientid = Guid.Parse(currentUserId);
 
-            var userpart = _context.EmployessUsers.Where(x => x.Id == _clientid).FirstOrDefault();
+
+
+
+
+            var Employeeinfo = await _context.EmployessUsers.Where(x => x.Id == _clientid).FirstOrDefaultAsync();
+
+
+            var teams = await _context.Teams.Where(x => x.Id == Employeeinfo.Employee_Team).FirstOrDefaultAsync();
+
             var validFilter = new PaginationFilter(filter.PageNumber, filter.PageSize);
-            var totalRecords = await _context.Tasks.Where(x => x.IsDelete == false && x.Task_part == userpart.Employee_Team && x.Task_Employee_WorkOn == _clientid).CountAsync();
+            var totalRecords = await _context.Tasks.Where(x => x.IsDelete == false && x.Task_part == teams.Id && x.Task_Status == 3).CountAsync();
 
 
-            var list = (from task in _context.Tasks.OrderByDescending(x => x.Task_Date).Where(x => x.IsDelete == false && x.Task_part == userpart.Employee_Team && x.Task_Employee_WorkOn == _clientid).AsNoTracking()
+            var list = (from task in _context.Tasks.OrderByDescending(x => x.Task_Date).Where(x => x.IsDelete == false && x.Task_part == teams.Id && x.Task_Status == 3).AsNoTracking()
                         join Employee1 in _context.EmployessUsers.AsNoTracking() on task.Task_Employee_Open equals Employee1.Id
-                        join tower in _context.Towers.AsNoTracking() on task.Tower_Id equals tower.Id
+                        join tower in _context.Towers.AsNoTracking() on task.Tower_Id equals tower.Id into vtower
+                        from tower in vtower.DefaultIfEmpty()
                         join Employee in _context.EmployessUsers.AsNoTracking() on task.Task_Employee_WorkOn equals Employee.Id into vEmployee
                         from Employee in vEmployee.DefaultIfEmpty()
                         join Employee2 in _context.EmployessUsers.AsNoTracking() on task.Task_Employee_Close equals Employee2.Id into vEmployee2
@@ -207,6 +292,9 @@ namespace hr_master.Controllers
                         join part in _context.Teams.AsNoTracking() on task.Task_part equals part.Id
                         join reward in _context.RewardsTable.AsNoTracking() on task.Task_Price_rewards equals reward.Id into vRewards
                         from reward in vRewards.DefaultIfEmpty()
+                        join internetuser in _context.InternetUsers on task.InternetUserId equals internetuser.Id into vinternetuser
+                        from internetuser in vinternetuser.DefaultIfEmpty()
+
 
                         select new TasksDto
                         {
@@ -227,14 +315,14 @@ namespace hr_master.Controllers
                             Tower_Name = tower.Tower_Name,
                             Tower_Id = tower.Id,
                             part_Id = part.Id,
-                            Task_Price = reward.RewardsPrice
+                            Task_Price = reward.RewardsPrice,
+                            InternetUserId = internetuser.Id
 
 
 
 
 
                         }).ToList();
-
 
             if (Task_Employee_WorkOn != null && Task_Employee_WorkOn != default)
                 list = list.Where(s => s.Task_Employee_WorkOn.Contains(Task_Employee_WorkOn)).ToList();
@@ -243,7 +331,7 @@ namespace hr_master.Controllers
                 list = list.Where(s => s.Task_Employee_Open.Contains(Task_Employee_Open)).ToList();
             totalRecords = list.Count();
             if (date != null && date != default)
-                list = list.Where(s => s.Task_Date.Date <= date).ToList();
+                list = list.Where(s => s.Task_Date.Date == date).ToList();
             totalRecords = list.Count();
             if (Task_Status != null && Task_Status != default)
                 list = list.Where(s => s.Task_Status == Task_Status).ToList();
@@ -355,6 +443,10 @@ namespace hr_master.Controllers
 
 
         }
+
+
+
+
         [HttpPut]
         public ActionResult<IEnumerable<string>> AddfollowerTask([FromBody] AddTaskFollowers form)
 
